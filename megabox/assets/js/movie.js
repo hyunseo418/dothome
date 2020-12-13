dc619a01dbff1cff7b37cf137c2b12aab51a0af6
hyunseo418 <69830073+hyunseo418@users.noreply.github.com>
2020-12-13 21:46:40 +0900

    2020.12.13

--- megabox/assets/js/movie.js
+++ megabox/assets/js/movie.js
@@ -3,18 +3,17 @@
     /* 트레일러 영상 플레이어를 활성화 */
     /* YouTube iframe API: https://developers.google.com/youtube/player_parameters */
     
-    (function handleTrailer() {
+   (function handleTrailer(){
         // 셀렉터 캐시
         var $selector = {
-            body: $( "body" ),
-            overlay: $( "#blackout" ),
-            modal: $( "#trailerModal" ),
-            showButton: $( "#showTrailer" ),
-            hideButton: $( "#hideTrailer" ),
+            body: $("body"),
+            overlay: $("#blackout"),
+            modal: $("#trailerModal"),
+            showButton: $("#showTrailer"),
+            hideButton: $("#hideTrailer")
         };
-        
-        // 플레이어
-        var player = {
+       // 플레이어
+       var player = {
             obj: null, // 플레이어 오브젝트
             query : {
                 theme: "dark",
@@ -31,11 +30,11 @@
         };
 
         // 보이기, 숨기기 버튼 활성화
-        $selector.showButton.on( "click", showPlayer );
-        $selector.hideButton.on( "click", hidePlayer );
+        $selector.showButton.on("click", showPlayer);
+        $selector.hideButton.on("click", hidePlayer);
 
-        // YouTube API를 이용해 iframe을 생성
-        function setPlayer( id ) {
+       // YouTube API를 이용해 iframe을 생성
+       function setPlayer( id ) {
             player.obj = new YT.Player( "trailer", {
                 width: 480,
                 height: 282,
@@ -52,44 +51,41 @@
             });
         }
 
-        // 화면 크기에 비례해 iframe의 크기 조절
-        function resizePlayer() {
+        function resizePlayer(){
             var viewport = {}, frame = {}, modal = {};
 
-            viewport.width = $( window ).width();
-            viewport.height = $( window ).height();
+            viewport.width = $(window).width();
+            viewport.height = $(window).height();
 
             frame.width = viewport.width;
-            frame.height = frame.width / 1.6; // 16 : 10
+            frame.height = frame.width / 1.6;   /* 16 : 10 비율 */
 
-            modal.top = ( ( viewport.height - frame.height ) / 2 ) + "px";
+            modal.top = ((viewport.height - frame.height) / 2) + "px";
             modal.left = "0px";
 
-            $selector.modal.css( modal );
+            $selector.modal.css(modal);
 
-            player.obj.setSize( frame.width, frame.height );
+            player.obj.setSize(frame.width, frame.height);
         }
 
         // iframe 보이기
-        function showPlayer() {
-            // 처음 클릭시 iframe을 생성 
-            if ( !player.obj ) {
-                setPlayer( $selector.showButton.data("youtube") );
+        function showPlayer(){
+            if(!player.obj){
+                setPlayer($selector.showButton.data("youtube"));
             }
-
             $selector.body.addClass("modal_on");
             $selector.overlay.show();
             player.visible = true;
         }
 
         // iframe 감추기
-        function hidePlayer() {
+        function hidePlayer(){
             player.obj.stopVideo();
             $selector.overlay.hide();
             $selector.body.removeClass("modal_on");
             player.visible = false;
         }
 
-    })();
 
+   })();
 })(jQuery);     
